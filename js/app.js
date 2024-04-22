e700dba4001dae05c3949a8fe5f46f13e3e5af76
eldhose-oss <eldhosebabu197@gmail.com>
2024-04-22 15:03:20 -0400

    completed parallax effect

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
 const parallax = document.querySelectorAll(".parallax");
 let xValue = 0, yValue = 0;
-
-window.addEventListener("mousemove",(event) => {
-    xValue = event.clientX - window.innerWidth / 2;
-    yValue = event.clientY - window.innerHeight / 2;
-
-    console.log(xValue , yValue);
+let rotateDegree = 0;
+function update(xvalue){
     parallax.forEach(element =>{
         let speedx = element.dataset.speedx;
         let speedy = element.dataset.speedy;
-        let zValue = event.clientX - parseFloat(getComputedStyle(element).left);
+        let speedz = element.dataset.speedz;
+        let rotationSpeed = element.dataset.rotation;
+        let isInLeft = (parseFloat(getComputedStyle(element).left) < window.innerWidth/2)?  1 : -1;
+        let zValue = (xvalue - parseFloat(getComputedStyle(element).left)) * isInLeft * 0.1;
         console.log(zValue)
-        element.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${-yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
+        element.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) 
+        rotateY(${rotateDegree * rotationSpeed}deg)
+        translateY(calc(-50% + ${-yValue * speedy}px)) perspective(2300px) 
+        translateZ(${zValue * speedz}px)`;
     })
+}
+update(0)
+window.addEventListener("mousemove",(event) => {
+    xValue = event.clientX - window.innerWidth / 2;
+    yValue = event.clientY - window.innerHeight / 2;
+    rotateDegree = (xValue / (window.innerWidth/2)) * 20;
+    console.log(xValue , yValue);
+    update(event.clientX);
 });
+//gsap animation
